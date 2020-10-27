8af483a608fdc7c7afeab9870fe79a9760ed98b5
emuswailit <emuswailit@gmail.com>
2020-10-27 09:17:32 +0300

    Splash activity done

--- app/src/main/java/ke/co/mobiticket/mobiticket/activities/SelectionActivity.java
+++ app/src/main/java/ke/co/mobiticket/mobiticket/activities/SelectionActivity.java
@@ -1,14 +1,16 @@
-package ke.co.mobiticket.mobiticket;
+package ke.co.mobiticket.mobiticket.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ke.co.mobiticket.mobiticket.R;
+
+public class SelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_selection);
     }
 }
---
+++ app/src/main/java/ke/co/mobiticket/mobiticket/activities/SplashActivity.java
@@ -0,0 +1,49 @@
+package ke.co.mobiticket.mobiticket.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import ke.co.mobiticket.mobiticket.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView mIVLogo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        initLayouts();
+        initializeListeners();
+    }
+
+    /* initialize listener */
+    private void initializeListeners() {
+        Glide.with(this).load(R.raw.ic_logo).into(mIVLogo);
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          startActivity(new Intent(SplashActivity.this, SelectionActivity.class));
+                                          onBackPressed();
+                                      }
+                                  },
+                3000);
+    }
+
+    /* init layout */
+    /* init layout */
+    private void initLayouts() {
+
+        mIVLogo = findViewById(R.id.ivLogo);
+    }
+}
