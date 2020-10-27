857ac9cc7aafdbf16dfdd346e447e97c1018a255
emuswailit <emuswailit@gmail.com>
2020-10-27 08:47:07 +0300

    Added nothing

---
+++ app/src/main/java/ke/co/mobiticket/mobiticket/MainActivity.java
@@ -0,0 +1,14 @@
+package ke.co.mobiticket.mobiticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
